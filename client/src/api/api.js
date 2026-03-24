b52eaa1275695d4432cbf4e464b0009b6d1c33b1
Farhan Faru <farhanfaru6458@gmail.com>
2026-03-24 12:26:33 +0530

    backend connected

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://shopco-ecommerce-1-6ccc.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
