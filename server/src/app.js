f809c5fa7ab10a9cfe45269991883a5584b30c27
Farhan Faru <farhanfaru6458@gmail.com>
2026-05-31 03:18:39 +0530

    enable seeders to populate new database

--- server/src/app.js
+++ server/src/app.js
@@ -101,15 +101,15 @@ app.use("/reviews", reviewRoutes);
     const { Product, DressStyle, Review } = require("./models");
     const productCount = await Product.count();
     if (productCount === 0) {
-      // await seedProducts();
-      // await seedDressStyles();
+      await seedProducts();
+      await seedDressStyles();
       console.log("Basic data seeded");
     }
 
     const reviewCount = await Review.count();
     if (reviewCount === 0) {
-      // await seedReviews();
-      console.log(" Reviews seeded");
+      await seedReviews();
+      console.log("Reviews seeded");
     }
 
     console.log("Database connected and synced");
